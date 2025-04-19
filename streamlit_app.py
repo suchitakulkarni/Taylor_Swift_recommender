3ef404b3f0944fcd687ed18cd65eb83f7bc7d190
Suchita Kulkarni <suchita.kulkarni@gmail.com>
2025-04-19 11:40:56 +0200

    layout modifications

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ with st.expander("Dataset"):
     for i in range(len(df['album'].unique().tolist())):
         album = df['album'].unique().tolist()[i]
         small_df.loc[i] = [album, len(df[df['album'] == album]['name'].tolist())]
-    st.dataframe(small_df, width = 500)
+    st.dataframe(small_df, width = 250)
     
     #for album in df['album'].unique().tolist():
     #    st.write("Number of songs per album:", album, len(df[df['album'] == album]['name'].tolist()))
