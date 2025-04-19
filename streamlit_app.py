b26b12e19adfcfa3678e5a7285590b31e4ce9cba
Suchita Kulkarni <suchita.kulkarni@gmail.com>
2025-04-19 11:31:56 +0200

    additional elements

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,8 +52,8 @@ with st.expander("Dataset"):
     st.dataframe(df_album_summary)
     st.write("Number of Albums analysed:", len(df['album'].unique().tolist()))
     st.write("Number of Songs analysed:", len(df['name'].tolist()))
-    
-    st.write("Number of songs per album:", len(df_album_summary['album'].tolist()))
+    for album in df['album'].unique().tolist():
+        st.write("Number of songs per album:", album, len(df[df['album'] == album].tolist()))
 
 # Divide page into two columns
 
